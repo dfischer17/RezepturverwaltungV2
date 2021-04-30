bee14f4a563eda34d7e28e6980ce3e96589b2adc
Pepe Fröhler <pefr2002@hotmail.com>
2021-04-30 13:04:14 +0200

    ViewModels moved to Program

---
+++ Program/Viewmodels/CustomerViewModel.cs
@@ -0,0 +1,104 @@
+﻿using Database;
+using Database.Entities;
+using MVVM.Tools;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace Viemodel
+{
+    public class CustomerViewModel : ObservableObject
+    {
+        private MyDbContext db;
+
+        public CustomerViewModel(MyDbContext db)
+        {
+            this.db = db;
+            Customers = db.Customers.AsObservableCollection();
+        }
+
+        public CustomerViewModel()
+        {
+
+        }
+
+        private ObservableCollection<Customer> customers;
+        private string lastnameTxt = "";
+        private string firstnameTxt = "";
+        private string phonenumber = "";
+        private string email = "";
+
+        /*Properties*/
+        public ObservableCollection<Customer> Customers
+        {
+            get { return customers; }
+            set
+            {
+                customers = value;
+                RaisePropertyChangedEvent(nameof(Customers));
+            }
+        }
+        
+        public string LastnameTxt
+        {
+            get { return lastnameTxt; }
+            set
+            {
+                lastnameTxt = value;
+                RaisePropertyChangedEvent(nameof(LastnameTxt));
+            }
+        }
+
+        public string FirstnameTxt
+        {
+            get { return firstnameTxt; }
+            set
+            {
+                firstnameTxt = value;
+                RaisePropertyChangedEvent(nameof(FirstnameTxt));
+            }
+        }
+
+        public string Phonenumber
+        {
+            get { return phonenumber; }
+            set
+            {
+                phonenumber = value;
+                RaisePropertyChangedEvent(nameof(Phonenumber));
+            }
+        }
+
+        public string EmailTxt
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                RaisePropertyChangedEvent(nameof(EmailTxt));
+            }
+        }
+
+        /*Commands*/
+        public ICommand AddCustomerCommand => new RelayCommand<string>(
+            AddCustomer,
+            x => LastnameTxt.Trim().Length > 0
+            );
+
+        /*/Helper*/
+        private void AddCustomer(string obj)
+        {
+            var customer = new Customer
+            {
+                Lastname = LastnameTxt,
+                Firstname = FirstnameTxt,
+                Phonenumber = long.Parse(Phonenumber),
+                Email = EmailTxt,
+            };
+
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            Customers = db.Customers.AsObservableCollection();
+        }
+    }
+}
---
+++ Program/Viewmodels/OrderViewModel.cs
@@ -0,0 +1,120 @@
+﻿using Database;
+using Database.Entities;
+using MVVM.Tools;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Viemodel
+{
+    public class OrderViewModel : ObservableObject
+    {
+        private readonly MyDbContext db;
+
+        public OrderViewModel()
+        {
+
+        }
+        public OrderViewModel(MyDbContext db)
+        {
+            this.db = db;
+            Customers = db.Customers.AsObservableCollection();
+        }
+
+        //Properties
+
+        private ObservableCollection<Customer> customers;
+
+        public ObservableCollection<Customer> Customers
+        {
+            get => customers;
+            set
+            {
+                customers = value;
+                RaisePropertyChangedEvent(nameof(Customers));
+            }
+        }
+        private Customer selectedCustomer;
+
+        public Customer SelectedCustomer
+        {
+            get => selectedCustomer;
+            set
+            {
+                selectedCustomer = value;
+                Orders = db.Orders.Where(x => x.CustomerId == SelectedCustomer.Id).AsObservableCollection();
+                Recipes = new ObservableCollection<Recipe>();
+                RaisePropertyChangedEvent(nameof(SelectedCustomer));
+            }
+        }
+
+        private ObservableCollection<Order> orders;
+
+        public ObservableCollection<Order> Orders
+        {
+            get => orders;
+            set
+            {
+                orders = value;
+                RaisePropertyChangedEvent(nameof(Orders));
+            }
+        }
+        private Order selectedOrder;
+
+        public Order SelectedOrder
+        {
+            get => selectedOrder;
+            set
+            {
+                selectedOrder = value;
+                if (selectedOrder != null)
+                {
+                    Recipes = db.OrderDetails.Where(x => x.OrderId == SelectedOrder.Id).Select(x => x.Recipe).AsObservableCollection();
+
+                }
+
+                RaisePropertyChangedEvent(nameof(SelectedOrder));
+            }
+        }
+        private ObservableCollection<Recipe> recipes;
+
+        public ObservableCollection<Recipe> Recipes
+        {
+            get => recipes;
+            set
+            {
+                recipes = value;
+                RaisePropertyChangedEvent(nameof(Recipes));
+            }
+        }
+
+        //Commands
+        public ICommand AddOrderCommand => new RelayCommand<string>(
+           AddOrder,
+           x => SelectedCustomer != null);
+
+        /*/Helper*/
+        private void AddOrder(string obj)
+        {
+            //Order
+            var order = new Order
+            {
+                CustomerId = SelectedCustomer.Id,
+                DeliveryDate = DateTime.Now,
+                OrderDate = DateTime.Now.AddDays(-1),
+            };
+            db.Orders.Add(order);
+            db.SaveChanges();
+            Orders = db.Orders.Where(x => x.CustomerId == SelectedCustomer.Id).AsObservableCollection();
+
+            //OrderDetail
+            //Implement
+        }
+
+
+    }
+}
